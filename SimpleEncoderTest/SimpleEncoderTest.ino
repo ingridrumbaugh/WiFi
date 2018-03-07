f49ff87b6d2b33ddbdd3e701d74283454f79c6a8
Ingrid Rumbaugh <rumbaugi@lafayette.edu>
2018-03-07 00:14:36 -0500

    encoders work

--- SimpleEncoderTest/SimpleEncoderTest.ino
+++ SimpleEncoderTest/SimpleEncoderTest.ino
@@ -1,3 +1,5 @@
+// EACH INTEGER IS 1/12 OF A TURN 
+
 // DRIVE MOTOR PINS
 #define pwm1 3
 #define pwm2 4
@@ -12,34 +14,54 @@
 #define enc1A 18
 #define enc1B 19
 
-int val; 
+#define enc2A 20
+#define enc2B 21
+
+int val1; 
 int enc1Pos = 0;
 int enc1ALast = LOW; 
-int n = LOW; 
+int n1 = LOW; 
+
+int val2;
+int enc2Pos = 0; 
+int enc2ALast = LOW;
+int n2 = LOW; 
 
 void setup() {
   pinMode(enc1A, INPUT); 
   pinMode(enc1B, INPUT); 
+  pinMode(enc2A, INPUT);
+  pinMode(enc2B, INPUT); 
   Serial.begin(9600); 
-
 }
 
 void loop() {
-  n = digitalRead(enc1A); 
-  if ((enc1ALast == LOW) && (n == HIGH)) {
-    
+  n2 = digitalRead(enc2A); 
+  n1 = digitalRead(enc1A); 
+  if ((enc1ALast == LOW) && (n1 == HIGH)) {
     if (digitalRead(enc1B) == LOW) {
-      
       enc1Pos--;
-      
     } else {
-      
       enc1Pos++; 
-      
     }
+    
+    Serial.println("Enc1: "); 
     Serial.print(enc1Pos);
     Serial.print("/"); 
+    Serial.println(" "); 
   }
-  enc1ALast = n; 
+  if ((enc2ALast == LOW) && (n2 == HIGH)) {
+    if (digitalRead(enc2B) == LOW) {
+      enc2Pos--;
+    } else {
+      enc2Pos++; 
+    }
+    Serial.println("Enc2: "); 
+    Serial.print(enc2Pos);
+    Serial.print("/"); 
+    Serial.println(" "); 
+  }
+  enc1ALast = n1; 
+  enc2ALast = n2; 
 
 }
